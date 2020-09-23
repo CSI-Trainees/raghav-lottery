4986ae15f5bcdd7a0272f4bd717de50948902638
Raghav goyal <raghavgoyaldbd478@gmail.com>
2020-09-23 21:47:09 +0530

    image add in winning section

--- script.js
+++ script.js
@@ -1,41 +1,27 @@
-var button= document.getElementById('btn');
-var num=document.getElementById('n.o');
+var button = document.getElementById("btn");
+var num = document.getElementById("n.o");
 
-
-button.addEventListener('click',function(max,min) {
+button.addEventListener("click", function (max, min) {
   var random = Math.floor(Math.random() * (30 - 10 + 1)) + 10;
-  num.innerText='your luckey number is'+" "+ random +" ";
-})
-button.addEventListener('click',  function disableButton() {
-        var btn = document.getElementById('btn');
-        btn.disabled = true;
-        
-    })
-var input=document.getElementById('check');
-var subbtn=document.getElementById('sumbit');
-var images=document.getElementById('winner-image')
-subbtn.addEventListener('click',function winner(){
-if(input.value==22||input.value==12||input.value==15){
-
-	var win1=document.getElementById('win');
-	var win2="you win"
-	win1.innerText=win2;
-	image.src=aa.jpg;
-	
-	
-
-
-
-}else{
-var loss1=document.getElementById('loss');
-	var loss2="you loss"
-	loss1.innerText=loss2;
-image.src=none;
-
-}
-
-
-
-
-
-})
+  num.innerText = " " +random + " ";
+});
+button.addEventListener("click", function disableButton() {
+  var btn = document.getElementById("btn");
+  btn.disabled = true;
+});
+var input = document.getElementById("check");
+var subbtn = document.getElementById("sumbit");
+var images = document.getElementById("winner-image");
+subbtn.addEventListener("click", function winner() {
+  if (input.value == 22 || input.value == 12 || input.value == 15) {
+    var win1 = document.getElementById("win");
+    var win2 = "you win";
+    win1.innerText = win2;
+    document.getElementById("winner-img").style.display = "block";
+  } else {
+    var loss1 = document.getElementById("loss");
+    var loss2 = " SORRY YOU LOSS";
+    loss1.innerText = loss2;
+    image.src = none;
+  }
+});
